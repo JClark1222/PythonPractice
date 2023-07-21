eec3f4a7987ed1addad647ce9d3eb0705d2944c0
JClark1222 <mlggeek@gmail.com>
2023-07-20 21:35:11 -0400

    first graph using sympy

---
+++ TestSpace.py
@@ -0,0 +1,8 @@
+def end_corona(recovers, new_cases, active_cases):
+    days = 0
+    while active_cases >= 0:
+        active_cases - recovers + new_cases
+        days = days + 1
+    print(days)
+ 
+end_corona(4000,2000,7000)       
